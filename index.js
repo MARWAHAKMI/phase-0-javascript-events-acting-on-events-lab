a6813ad7c85dfdcd06c85068ce56ec1c95a762f1
Marwa <marwahakmi@gmail.com>
2022-08-05 11:35:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 // Your code here
+// function moveDodgerLeft() {
+//     const dodger = document.getElementById('dodger')
+//     dodger.style.left = '80px'
+// }
+
+// function moveDodgerRight() {
+//     const dodger = document.getElementById('dodger')
+//     dodger.style.left = '300px'
+// }
+
+
+// moveDodgerLeft()
+// moveDodgerRight()
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+})
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
 function moveDodgerLeft() {
-    const dodger = document.getElementById('dodger')
-    dodger.style.left = '80px'
+    const l = dodger.style.left.replace("px", "")
+    const left = parseInt(l, 10)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
 }
-
 function moveDodgerRight() {
-    const dodger = document.getElementById('dodger')
-    dodger.style.left = '300px'
+    const l = dodger.style.left.replace("px", "")
+    const left = parseInt(l, 10)
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`
+    }
 }
-
-
-moveDodgerLeft()
-moveDodgerRight()
