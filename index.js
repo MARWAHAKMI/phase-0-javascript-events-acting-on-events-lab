1085563aa1f275716d770bc15e25ba8016dd81c9
Marwa <marwahakmi@gmail.com>
2022-08-05 11:27:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger')
+    dodger.style.left = '80px'
+}
+
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger')
+    dodger.style.left = '300px'
+}
+
+
+moveDodgerLeft()
+moveDodgerRight()
